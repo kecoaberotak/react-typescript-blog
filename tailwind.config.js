1eede1b365627237345084c16b53607798353a9e
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-02-06 15:47:32 +0700

    edit css pake tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'tosca': "#2cb786",
+        'abu-gelap': '#322f2f',
+      }
+    },
   },
   plugins: [],
 }
